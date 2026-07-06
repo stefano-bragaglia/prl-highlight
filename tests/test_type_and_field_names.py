1f3ceaaf540869aad53888cda6cd46e041cc7883
Stefano Bragaglia <stefano-bragaglia@users.noreply.github.com>
2026-07-06 14:33:18 +0100

    Test: type-and-field-names scope tests

---
+++ tests/test_type_and_field_names.py
@@ -0,0 +1,34 @@
+from .helpers import has_scope, parse
+
+
+def test_declare_type_name_and_field():
+    parsed = parse("declare Temperature\n  value: float\nend\n")
+    assert has_scope(parsed, "entity.name.type.prl", "Temperature")
+    assert has_scope(parsed, "variable.other.property.prl", "value")
+    assert has_scope(parsed, "entity.name.type.prl", "float")
+
+
+def test_declare_extends_both_types():
+    parsed = parse("declare Dog extends Animal\n  breed: str\nend\n")
+    assert has_scope(parsed, "entity.name.type.prl", "Dog")
+    assert has_scope(parsed, "entity.name.type.prl", "Animal")
+
+
+def test_generic_field_type():
+    parsed = parse("declare Foo\n  items: List<Integer>\nend\n")
+    assert has_scope(parsed, "entity.name.type.prl", "List")
+    assert has_scope(parsed, "entity.name.type.prl", "Integer")
+    assert has_scope(parsed, "variable.other.property.prl", "items")
+
+
+def test_pattern_head_type_name():
+    parsed = parse('rule "x"\nwhen\n  Temperature($t: value)\nthen\nend\n')
+    assert has_scope(parsed, "entity.name.type.prl", "Temperature")
+
+
+def test_import_names_are_types():
+    parsed = parse(
+        "from examples.declarative.domain import Vehicle, Fleet\n"
+    )
+    assert has_scope(parsed, "entity.name.type.prl", "Vehicle")
+    assert has_scope(parsed, "entity.name.type.prl", "Fleet")
